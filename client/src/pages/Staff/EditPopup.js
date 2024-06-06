8e97aff8ec08f5edb5080a5d3ca3897d445f1109
quagthanh <dinhquangthanh11@gmail.com>
2024-06-06 13:47:27 +0700

    update change from thien branch

---
+++ client/src/pages/Staff/EditPopup.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './EditPopup.css'
+import upload from '../../assets/upload.svg'
+function EditPopup(props) {
+    const { staff } = props;
+    return (props.trigger)?(
+        <div className='edit-popup'>
+        <div className='edit-popup-inner'>
+          {/* // <button className='btn-add-sp' onClick={() => props.setTrigger(false)}>Them</button> */}
+          <div className='icon-close'><button id='close-icon' onClick={() => props.setTrigger(false)}>X</button></div>
+          <div className='edit-title-popup'>EDIT STAFFS PROFILE</div>
+          <div className='edit-staff-content'>
+            <div className='edit-staff-form'>
+              <form className='form-edit'>
+  
+                <div className='form-item-edit'><label for="input1">Name</label>
+                  <input type='text' placeholder='david ngo'></input></div>
+  
+                <div className='form-item-edit'><label for="input1">ID num</label>
+                  <input type='text' placeholder='090979770'></input></div>
+  
+                <div className='form-item-edit'><label for="input1">Phone</label>
+                  <input type='text' placeholder='78070870780'></input></div>
+  
+                <div className='form-item-edit'><label for="input1">Email</label>
+                  <input type='text' placeholder='davidngo@gmail.com'></input></div>
+  
+                <div className='form-item-edit'><label for="input1">Salary</label>
+                  <input type='text' placeholder='2000$'></input></div>
+
+              </form>
+            </div>
+            <div className='upload-image-edit-staff'>
+              <div className='form-item-image'> Image</div>
+              <div className='upload-item'><img src={upload}></img></div>
+            </div>
+          </div>
+          <div className='footer-edit-staff'>
+            <div className='btn-edit'>
+              <button className='btn-del' onClick={() => props.setTrigger(false)}>DELETE</button>
+              <button className='btn-edit' onClick={() => props.setTrigger(false)}>SAVE</button>
+              </div>
+            </div>
+          {props.children}
+        </div>
+      </div>
+      ):"";
+    }
+
+export default EditPopup
--- client/src/pages/Staff/Staff.js
+++ client/src/pages/Staff/Staff.js
@@ -1,18 +1,181 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './Staff.css';
+import AddPopup from "./AddPopup";
+import EditPopup from "./EditPopup";
+import DeletePopup from "./DeletePopup";
 import icon_search from '../../assets/glass.svg';
 import pen_edit from '../../assets/pen.svg';
 import trash from '../../assets/trash.svg';
 //'react-bootstrap-table/css/react-bootstrap-table.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link, useAsyncError } from "react-router-dom";
+import axios from 'axios'
+
+const aray = [
+    {
+        id: 1,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0,
+
+    },
+    {
+        id: 2,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 3,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 4,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 5,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 6,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 7,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 8,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 9,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 10,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 11,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 12,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 13,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 14,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 15,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    },
+    {
+        id: 16,
+        name: "Tony B",
+        email: "example@gmail.com",
+        phone: "0909954546",
+        role: "Dược sĩ",
+        present: 0
+    }
+
+]
+
 function Staff() {
+    const [data,setData]=useState([])
     useEffect(() => {
-        document.title = "Staff Management"
+        setStaff(aray.filter(item => item.present === 0));
+        
+        axios.get('http://localhost:8081/') 
+        .then(res=>setData(res.data))
+        .catch(err=>console.log(err));
+        
+            document.title = "Staff Management"
     })
+    const [staff, setStaff] = useState([]);
+    const [addPopup, setAddPopup] = useState(false);
+    const [editPopup, setEditPopup] = useState(false);
+    const [deletePopup, setDeletePopup] = useState(false);
+
+    
+
+   
     return (
         <React.Fragment>
             <h1>STAFFS</h1>
             <div className="staff-parent">
+                <AddPopup trigger={addPopup} setTrigger={setAddPopup} >
+                    
+                </AddPopup>
+                <EditPopup trigger={editPopup} setTrigger={setEditPopup} >
+                    
+                </EditPopup>
+                <DeletePopup trigger={deletePopup} setTrigger={setDeletePopup} >
+                    
+                </DeletePopup>
                 <div className="staff-bg">
                     <div className="search-wrapper">
                         <div className="nav-search">
@@ -21,27 +184,12 @@ function Staff() {
                                 <img src={icon_search} ></img>
                             </div>
                         </div>
-                        <div className="staff-button-add">Add Employer</div>
+                        <div className="staff-button-add"><button onClick={()=>setAddPopup(true)}>Add Employer</button></div>
                     </div>
                     <div className="staff-list">
-                        {/* <div className="child-list">
-                            <table className="table">
-                            <thead class="table-light">
-                                <tr>
-                                    <th>dadad</th>
-                                    <th>Id</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Phone</th>
-                                    <th>Role</th>
-                                    <th>Action</th>
-                                </tr>
-                                </thead>
-                            </table>
-                        </div> */}
                         <div className="staff-list-context">
-                            <table class="table">
-                                <thead class="table-light" >
+                            <table class="table ">
+                                <thead class="table" >
                                     <tr >
                                         <th>
                                             <input className="checkbox-staff" type="checkbox"></input>
@@ -56,181 +204,27 @@ function Staff() {
                                 </thead>
                                 <tbody>
 
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>001</td>
-                                        <td>Tony A</td>
-                                        <td>example@gmail.com</td>
-                                        <td>09341841</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>002</td>
-                                        <td>Tony B</td>
-                                        <td>example@gmail.com</td>
-                                        <td>8301413</td>
-                                        <td>Quản lý</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>003</td>
-                                        <td>Tony C</td>
-                                        <td>example@gmail.com</td>
-                                        <td>987384783</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>004</td>
-                                        <td>Tony D</td>
-                                        <td>example@gmail.com</td>
-                                        <td>387140134</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>005</td>
-                                        <td>Tony E</td>
-                                        <td>example@gmail.com</td>
-                                        <td>458245254</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>006</td>
-                                        <td>Tony F</td>
-                                        <td>example@gmail.com</td>
-                                        <td>90482555</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>007</td>
-                                        <td>Tony G</td>
-                                        <td>example@gmail.com</td>
-                                        <td>082254</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>008</td>
-                                        <td>Tony H</td>
-                                        <td>example@gmail.com</td>
-                                        <td>458745</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>009</td>
-                                        <td>Tony I</td>
-                                        <td>example@gmail.com</td>
-                                        <td>48528452</td>
-                                        <td>Dược sĩ</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>010</td>
-                                        <td>Tony J</td>
-                                        <td>example@gmail.com</td>
-                                        <td>425725</td>
-                                        <td>Nhân viên kho </td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        <td>Otto</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        <td>Otto</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
-                                    <tr>
-
-                                        <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        <td>Otto</td>
-                                        <td><div className="icon-action">
-                                            <img src={pen_edit}></img>
-                                            <img src={trash}></img>
-                                        </div></td>
-                                    </tr>
+                                    {data.map((staff,index)=> {
+                                        return <tr key={index}>
+                                            <td><div><input className="checkbox-staff" type="checkbox"></input></div></td>
+                                            <td>{staff.id}</td>
+                                            <td>{staff.name}</td>
+                                            <td>{staff.email}</td>
+                                            <td>{staff.phone}</td>
+                                            <td>{staff.role}</td>
+                                            <td><div className="icon-action">
+                                                <div onClick={() => setEditPopup(true)}><button><img src={pen_edit}></img></button></div>
+                                                <div onClick={() => setDeletePopup(true)}><button><img src={trash}></img></button></div>
+                                            </div></td>
+                                        </tr>
+                                    })}
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 </div>
             </div>
+
 
         </React.Fragment>
     )
